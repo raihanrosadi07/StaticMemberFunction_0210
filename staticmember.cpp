95fc1649d767667559e72ee84fcef30fddc72fab
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-04 19:06:47 +0700

    class mahasiswa public

--- staticmember.cpp
+++ staticmember.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa {
+
+public:
+    static int main;
+    int id;
+    string nama;
+
+    void setID();
+    void prinALL();
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    }
+};
