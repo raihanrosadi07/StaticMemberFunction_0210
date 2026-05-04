859c82367d029b94867b33fc7dfb0cf1643f126c
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-04 19:29:21 +0700

    int main

--- staticmember.cpp
+++ staticmember.cpp
@@ -27,3 +27,18 @@ void Mahasiswa::printALL() {
     cout <<"ID : " << id << endl;
     cout << "Nama : " << nama << endl;
 }
+
+int main() {
+    Mahasiswa mhs1("Megawati");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Purbaya");
+    Mahasiwa mhs4("Joko widodo");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printALl();
+
+    return 0;
+    
+}
