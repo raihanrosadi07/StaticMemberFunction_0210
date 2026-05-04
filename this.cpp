40b7b0eb2fb51dfff29dce3916446e99a428c28f
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-04 18:36:26 +0700

    class

--- this.cpp
+++ this.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+class buku{
+    string judul;
+    public:
+        string setget(string judul){
+            this->judul= judul;
+            return this->judul;
+        }
+};
